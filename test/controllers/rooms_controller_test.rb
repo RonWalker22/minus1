4a7ee7ab7de39fe0854e7de0019d908a799dec06
Ronald Walker <rondwalker22@gmail.com>
2019-01-20 09:12:27 -0800

    adds RoomsController

---
+++ test/controllers/rooms_controller_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class RoomsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @room = rooms(:one)
+    @operator = operators(:one)
+    sign_in @operator
+  end
+
+  test 'should get index' do
+    get rooms_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_room_url
+    assert_response :success
+  end
+
+  test 'should create room' do
+    assert_difference('Room.count') do
+      post rooms_url, params: { room: { name: 'New Room',
+                                        private: false,
+                                        override_preference: true } }
+    end
+
+    assert_redirected_to room_url(Room.last)
+  end
+
+  test 'should show room' do
+    get room_url(@room)
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_room_url(@room)
+    assert_response :success
+  end
+
+  test 'should update room' do
+    patch room_url(@room), params: { room: { name: 'New Room2' } }
+    assert_redirected_to room_url(@room)
+  end
+
+  test 'should destroy room' do
+    assert_difference('Room.count', -1) do
+      delete room_url(@room)
+    end
+
+    assert_redirected_to rooms_url
+  end
+end
