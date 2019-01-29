1d2eab17ac06c1e5c05605a0ee4b1eddf99be274
Ronald Walker <rondwalker22@gmail.com>
2019-01-29 06:07:43 -0800

    Adds new_recipe link to objectives#show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :set_selected_recipe, only: [:new]
 
   # GET /recipes
   # GET /recipes.json
@@ -70,5 +71,9 @@ class RecipesController < ApplicationController
     def recipe_params
       params[:recipe][:commander_id] = current_operator.id
       params.fetch(:recipe, {}).permit(:name, :objective_id, :commander_id)
+    end
+
+    def set_selected_recipe
+      @selected_objective = params[:obj].to_i
     end
 end
