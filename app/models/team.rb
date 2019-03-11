4fbfdd0537602b366df381286a76ed3b5dc8b39f
Ronald Walker <rondwalker22@gmail.com>
2019-03-10 21:22:50 -0700

    Adds commander to list of team memebers after creation of team

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,11 @@ class Team < ApplicationRecord
   belongs_to :commander, class_name: 'Operator'
   has_many :operator_teams
   has_many :operators, through: :operator_teams
+  after_create :add_commander_to_memebers
+
+  private
+
+    def add_commander_to_memebers
+      operators << commander
+    end
 end
