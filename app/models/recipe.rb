9d2d66915e97dd62b79547745fb576d27ef16337
Ronald Walker <rondwalker22@gmail.com>
2019-03-23 13:34:11 -0700

    Add cascade delete to Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :commander, class_name: 'Operator'
   belongs_to :objective, optional: true
-  has_many :directions
-  has_many :ingredients
-  has_many :operator_recipes
+  has_many :directions, dependent: :delete_all
+  has_many :ingredients, dependent: :delete_all
+  has_many :operator_recipes, dependent: :delete_all
   has_many :operators, through: :operator_recipes
 end
