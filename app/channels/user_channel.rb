3034cc4b94d172d5ea11048362cf39006fd211b0
Ronald Walker <rondwalker22@gmail.com>
2019-04-13 17:18:29 -0700

    Use update_attributes method to update current_user

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -1,13 +1,11 @@
 class UserChannel < ApplicationCable::Channel
   def subscribed
-    current_user.online = true
-    current_user.save
+    current_user.update_attributes online: true
     stream_for current_user
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-    current_user.online = false
-    current_user.save
+    current_user.update_attributes online: false
   end
 end
