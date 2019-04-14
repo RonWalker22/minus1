a6e0abdf389035db8f62bc655a2735c1f7c0c164
Ronald Walker <rondwalker22@gmail.com>
2019-04-13 17:35:48 -0700

    Add 'kill_counter' and 'active' attributes to Room

--- db/migrate/20190107191057_create_rooms.rb
+++ db/migrate/20190107191057_create_rooms.rb
@@ -5,6 +5,8 @@ class CreateRooms < ActiveRecord::Migration[5.2]
                    foreign_key: { to_table: :users },
                    null: false
       t.references :strategy
+      t.integer :kill_counter, default: 0, null: false
+      t.boolean :active, default: false, null: false
       t.string :name, null: false
       t.timestamps
       t.boolean :private, default: false, null: false
